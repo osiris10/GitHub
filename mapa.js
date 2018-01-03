a67f02b3d21c8470f7c19af78f19e9012affbcd7
osiris10 <35061305+osiris10@users.noreply.github.com>
2018-01-03 11:11:59 +0100

    Add files via upload

---
+++ mapa.js
@@ -0,0 +1,126 @@
+ 
+      function initMap() {
+	
+		
+	  
+  var a001 = {
+  info: '<strong>HER“A DOUBLE STAR RADVA“ PARK</strong><br>\Zvolensk· cesta<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.70789,  19.138513">Navigovaù</a>',lat: 48.70789, long:  19.138513};
+  var a002 = {
+  info: '<strong>HER“A PALACE GAMES CLUB</strong><br>\äancov·<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.157546, 17.125969">Navigovaù</a>',lat: 48.157546, long: 17.125969};
+  var a003 = {
+  info: '<strong>HER“A MAGIC GAMES</strong><br>\Sl·dkoviËova<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=49.438395, 18.784073">Navigovaù</a>',lat: 49.438395, long: 18.784073};
+  var a004 = {
+  info: '<strong>HER“A RIO</strong><br>\N·mestie slobody<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.933597, 21.910375">Navigovaù</a>',lat: 48.933597, long: 21.910375};
+  var a005 = {
+  info: '<strong>HER“A PROGRAM</strong><br>\Z·mock·<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.989391, 18.219651">Navigovaù</a>',lat: 48.989391, long: 18.219651};
+  var a006 = {
+  info: '<strong>HER“A BAR LAS VEGAS</strong><br>\1. m·ja<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=49.301736, 18.782799">Navigovaù</a>',lat: 49.301736, long: 18.782799};
+  var a007 = {
+  info: '<strong>HER“A VEGAS</strong><br>\BelopockÈho<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=49.080906, 19.612249">Navigovaù</a>',lat: 49.080906, long: 19.612249};
+  var a008 = {
+  info: '<strong>HER“A VICTORY</strong><br>\Dr. Herza<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=, ">Navigovaù</a>',lat: 48.327540, long: 19.666336 };
+  var a009 = {
+  info: '<strong>HER“A RED CAF…</strong><br>\HaliËsk· cesta<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.332134, 19.640773">Navigovaù</a>',lat: 48.332134, long: 19.640773};
+  var a010 = {
+  info: '<strong>HER“A PROGRAM</strong><br>\Z·mock·<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.438521, 17.025578">Navigovaù</a>',lat: 48.438521, long: 17.025578};
+  var a011 = {
+  info: '<strong>HER“A RIO</strong><br>\N·mestie osloboditeæov<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.756294, 21.918112">Navigovaù</a>',lat: 48.756294, long: 21.918112};
+  var a012 = {
+  info: '<strong>HER“A REGENA</strong><br>\SNP<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.936850, 18.143382">Navigovaù</a>',lat: 48.936850, long: 18.143382};
+  var a013 = {
+  info: '<strong>HER“A VICT”RIA</strong><br>\MierovÈ n·mestie <br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.937125, 18.146726">Navigovaù</a>',lat: 48.937125, long: 18.146726};
+  var a014 = {
+  info: '<strong>HER“A VABANK</strong><br>\N·breûn·<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=47.977746, 18.166112">Navigovaù</a>',lat: 47.977746, long: 18.166112};
+  var a015 = {
+  info: '<strong>HER“A KAZA»OK </strong><br>\J. Z·borskÈho<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.588479, 17.819481">Navigovaù</a>',lat: 48.588479, long: 17.819481};
+  var a016 = {
+  info: '<strong>HER“A ADMIR¡L CLUB</strong><br>\Skl·rska<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.428209, 19.788011">Navigovaù</a>',lat: 48.428209, long: 19.788011};
+  var a017 = {
+  info: '<strong>HER“A DOUBLE STAR</strong><br>\N·mestie Legion·rov<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.993595, 21.244301">Navigovaù</a>',lat: 48.993595, long: 21.244301};
+  var a018 = {
+  info: '<strong>HER“A MAGIC</strong><br>\Hlavn·<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.271704, 17.031988">Navigovaù</a>',lat: 48.271704, long: 17.031988};
+  var a019 = {
+  info: '<strong>HER“A DOUBLE STAR</strong><br>\Stropkovsk· <br>\<a href="https://www.google.com/maps/dir/?api=1&destination=49.304160, 21.570913">Navigovaù</a>',lat: 49.304160, long: 21.570913};
+  var a020 = {
+  info: '<strong>HER“A DOUBLESTAR</strong><br>\Hlavn·<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.150234, 17.873480">Navigovaù</a>',lat: 48.150234, long: 17.873480};
+  var a021 = {
+  info: '<strong>HER“A PARK PROGRAM</strong><br>\Kragujevsk˝ch hrdinov<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.895517, 18.049365">Navigovaù</a>',lat: 48.895517, long: 18.049365};
+  var a022 = {
+  info: '<strong>HER“A CLUB Ñ7ì</strong><br>\Gener·la Svobodu <br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.874223, 18.046922">Navigovaù</a>',lat: 48.874223, long: 18.046922};
+  var a023 = {
+  info: '<strong>HER“A BLACK JACK</strong><br>\PalackÈho<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.895728, 18.040313">Navigovaù</a>',lat: 48.895728, long: 18.040313};
+  var a024 = {
+  info: '<strong>TROMBETTA CAFF… - HER“A</strong><br>\Zlatovsk· <br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.894778, 18.021518">Navigovaù</a>',lat: 48.894778, long: 18.021518};
+  var a025 = {
+  info: '<strong>HER“A MONACO CLUB</strong><br>\VavreËka<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=, ">Navigovaù</a>',lat: 49.394338, long: 19.481989};
+  var a026 = {
+  info: '<strong>HER“A RIO</strong><br>\Ondavsk· <br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.886295, 21.683572">Navigovaù</a>',lat: 48.886295, long: 21.683572};
+  var a027 = {
+  info: '<strong>HER“A DOUBLESTAR</strong><br>\Svitavsk·<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=48.594435, 18.844287">Navigovaù</a>',lat: 48.594435, long: 18.844287};
+  var a028 = {
+  info: '<strong>HER“A SPEEDFIRE</strong><br>\Vojtecha TvrdÈho<br>\<a href="https://www.google.com/maps/dir/?api=1&destination=49.225128, 18.745670">Navigovaù</a>',lat: 49.225128, long: 18.745670};
+
+
+	var locations = [
+      	  [a001.info, a001.lat, a001.long, 1],
+[a002.info, a002.lat, a002.long, 2],
+[a003.info, a003.lat, a003.long, 3],
+[a004.info, a004.lat, a004.long, 4],
+[a005.info, a005.lat, a005.long, 5],
+[a006.info, a006.lat, a006.long, 6],
+[a007.info, a007.lat, a007.long, 7],
+[a008.info, a008.lat, a008.long, 8],
+[a009.info, a009.lat, a009.long, 9],
+[a010.info, a010.lat, a010.long, 10],
+[a011.info, a011.lat, a011.long, 11],
+[a012.info, a012.lat, a012.long, 12],
+[a013.info, a013.lat, a013.long, 13],
+[a014.info, a014.lat, a014.long, 14],
+[a015.info, a015.lat, a015.long, 15],
+[a016.info, a016.lat, a016.long, 16],
+[a017.info, a017.lat, a017.long, 17],
+[a018.info, a018.lat, a018.long, 18],
+[a019.info, a019.lat, a019.long, 19],
+[a020.info, a020.lat, a020.long, 20],
+[a021.info, a021.lat, a021.long, 21],
+[a022.info, a022.lat, a022.long, 22],
+[a023.info, a023.lat, a023.long, 23],
+[a024.info, a024.lat, a024.long, 24],
+[a025.info, a025.lat, a025.long, 25],
+[a026.info, a026.lat, a026.long, 26],
+[a027.info, a027.lat, a027.long, 27],
+[a028.info, a028.lat, a028.long, 28]
+
+
+
+    ];
+	
+	
+	
+
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 8,
+		center: new google.maps.LatLng(48.70789,  19.138513),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	var infowindow = new google.maps.InfoWindow({});
+
+	var marker, i;
+
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		});
+
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i][0]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
+}
+ 
